41ebec41fd153d0f484ba5823aeceb1ac68fc3d9
Teru Kaazhooft <n401571@gymnasiumnovum.nl>
2023-11-16 18:09:34 +0100

    a

---
+++ 1.py
@@ -0,0 +1,36 @@
+# print(False.__dir__().__doc__.__len__()) # 141
+# print(True.__class__.__flags__) #21500160
+
+# print(ord(False.__dir__().__doc__[2]))
+
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__[False.__dir__().__doc__.__len__()])
+
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[22][1])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__[2])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[4][5])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[4][6])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__[1])
+
+# >>> None.__class__.__doc__.__le__(1).__str__().split()[0][1]
+# 'o' -> 111
+
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[22][1])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__[ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][6].upper())])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[eval.__class__.__dir__(1).__doc__.split()[-6:][0].__len__()][5])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[eval.__class__.__dir__(1).__doc__.split()[-6:][0].__len__()][6])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__[ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower())])
+
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[22][1])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__[ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][(int(str(7836482 >> 4)[3]) - (ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower())))].upper())])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[eval.__class__.__dir__(1).__doc__.split()[-(int(str(7836482 >> 4)[3]) - (ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower()))):][0].__len__()][80 >> 4])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[eval.__class__.__dir__(1).__doc__.split()[-(int(str(7836482 >> 4)[3]) - (ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower()))):][0].__len__()][(int(str(7836482 >> 4)[3]) - (ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower())))])
+# print(__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__[ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower())])
+
+eval(
+    eval("__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[22][1]") +
+    eval("__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__[ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][(int(str(7836482 >> 4)[3]) - (ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower())))].upper())]") +
+    eval("__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[eval.__class__.__dir__(1).__doc__.split()[-(int(str(7836482 >> 4)[3]) - (ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower()))):][0].__len__()][80 >> 4]") +
+    eval("__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__.split()[eval.__class__.__dir__(1).__doc__.split()[-(int(str(7836482 >> 4)[3]) - (ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower()))):][0].__len__()][(int(str(7836482 >> 4)[3]) - (ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower())))]") +
+    eval("__builtins__.__dir__()[True.__class__.__flags__ - 21500144].__doc__[ord(None.__class__.__doc__.__le__(1).__str__().split()[0][1]) - ord(None.__class__.__doc__.__le__(1).__str__().split()[0][0].lower())]") +
+        "('Hello, world!')"
+)
